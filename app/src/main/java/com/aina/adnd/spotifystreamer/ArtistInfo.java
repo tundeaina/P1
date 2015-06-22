e70248074cfd84f3bcba791f147a21b53b8763b4
Tunde Aina <tundeaina@gmail.com>
2015-06-22 01:19:10 -0400

    Preserve state on Rotate and Up Navigation

--- app/src/main/java/com/aina/adnd/spotifystreamer/ArtistInfo.java
+++ app/src/main/java/com/aina/adnd/spotifystreamer/ArtistInfo.java
@@ -1,10 +1,25 @@
 package com.aina.adnd.spotifystreamer;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Tunde Aina on 6/20/2015.
+ * http://developer.android.com/reference/android/os/Parcelable.html
  */
-public class ArtistInfo {
 
+public class ArtistInfo implements Parcelable {
+
+    public static final Parcelable.Creator<ArtistInfo> CREATOR
+            = new Parcelable.Creator<ArtistInfo>() {
+        public ArtistInfo createFromParcel(Parcel in) {
+            return new ArtistInfo(in);
+        }
+
+        public ArtistInfo[] newArray(int size) {
+            return new ArtistInfo[size];
+        }
+    };
     private String Id;
     private String Name;
     private String ImageUrl;
@@ -18,6 +33,12 @@ public class ArtistInfo {
         this.Name = name;
         this.ImageUrl = imgurl;
 
+    }
+
+    private ArtistInfo(Parcel in) {
+        Id = in.readString();
+        Name = in.readString();
+        ImageUrl = in.readString();
     }
 
     public String getId() {
@@ -42,6 +63,17 @@ public class ArtistInfo {
 
     public void setImageUrl(String imgurl) {
         this.ImageUrl = imgurl;
+    }
+
+    public int describeContents() {
+        return 0;
+    }
+
+    public void writeToParcel(Parcel out, int flags) {
+
+        out.writeString(Id);
+        out.writeString(Name);
+        out.writeString(ImageUrl);
     }
 
 }
